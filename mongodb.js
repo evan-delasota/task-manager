656a0fe91282b118d33ffcd4e4d70c42333474f2
Evan Delasota <evan_delasota@yahoo.com>
2021-08-10 17:27:45 -0700

    Implemeneted mongodb.js to connect to local db

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'task-manager';
+
+// Connect to db server
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+         return console.log('Unable to connect to database.');
+    }
+    
+    const db = client.db(dbName);
+
+    db.collection('users').insertOne({
+        name: 'Evan',
+        age: 26
+    });
+});
