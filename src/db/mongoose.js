9ff823633e6db03d4b44da44c71af4a36b9d3a49
Evan Delasota <evan_delasota@yahoo.com>
2021-08-14 15:49:45 -0700

    Installed validator, added user model with validators

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -7,7 +8,44 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        trim: true,
+        validate(value) {
+            if (value.includes('password')) {
+                throw new Error('Password cannot be "password"');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email address');
+            }
+        }
 
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (!validator.isInt(value) || value < 0) {
+                throw new Error('Invalid age input');
+            } 
+        }
+    }    
+});
 const Task = mongoose.model('Task', {
     description: {
         type: String
