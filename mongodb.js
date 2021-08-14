72e32df3f1c9f630c6ccbdf7220cab1ca29b3ecd
Evan Delasota <evan_delasota@yahoo.com>
2021-08-14 02:37:52 -0700

    update example

--- mongodb.js
+++ mongodb.js
@@ -2,9 +2,6 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
-const id = new ObjectId();
-console.log(id);
-console.log(id.getTimestamp());
 
 // Connect to db server
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
@@ -14,51 +11,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(dbName);
 
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Evan',
-    //     age: 26
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Could not insert user');
-    //     }
-
-    //     console.log(result.ops);
-    // });
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 21
-    //     }, 
-    //     {
-    //         name: 'Evan',
-    //         age: 26
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Could not insert user');
-    //     }
-
-    //     console.log(result.ops);
-    // });
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Make bed',
-            completed: false
-        }, {
-            description: 'Complete this project',
-            completed: false
-        }, {
-            description: 'Drink 3 liters of water',
-            completed: false
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
         }
-    ], (error, result)=> {
-        if (error) {
-            return console.log('Could not insert tasks');
-        }
-
-        console.log(result.ops);
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     });
 });
