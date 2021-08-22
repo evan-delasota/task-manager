5650d83585b78d3206a0f8edab0fb36c2908edac
Evan Delasota <evan_delasota@yahoo.com>
2021-08-21 21:17:12 -0700

    Modified task patch method to accomodate middleware

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -58,7 +58,12 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(id, requestedUpdate, { new: true, runValidators: true });
+        const task = await Task.findById(id);
+
+        toBeUpdated.forEach((update) => {
+            task[update] = requestedUpdate[update];
+        });
+        await task.save();
 
         if (!task) {
             return res.status(404).send();
